86a04bab4647cf1cfb06c6f26778f8355708369b
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-01-10 10:53:51 -0300

    feature(helloworld): first try with react

---
+++ app/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloWorld extends React.Component{
+	render () {
+		return (
+			<h1>¡Hello World!, from React</h1>
+		);
+	}
+}
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
